926fbbf94d6864ce9535ec79175f3834a5010348
ChelseyReann <chelseyreann@gmail.com>
2023-05-22 14:41:28 -0400

    solves first problem

--- twoSum.js
+++ twoSum.js
@@ -7,3 +7,29 @@
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums, target) => {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
+
+// const nums = [2, 7, 11, 15]
+// const target = 9
+// const result = twoSum(nums, target)
+// console.log(result)
+
+// const nums = [3, 2, 4]
+// const target = 6
+// const result = twoSum(nums, target)
+// console.log(result)
+
+// const nums = [3, 3]
+// const target = 6
+// const result = twoSum(nums, target)
+// console.log(result)
