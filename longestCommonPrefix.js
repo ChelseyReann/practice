7d669f4bd9979ffbc2fb55e8a20a9387256ff2ab
ChelseyReann <chelseyreann@gmail.com>
2023-05-23 14:24:20 -0400

    adds solution for latest problem

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -3,5 +3,20 @@
 // If there is no common prefix, return an empty string "".
 
 const longestCommonPrefix = (strs) => {
-    
+    if (strs.length === 0) {
+        return ""
+    }
+
+    return strs.reduce((prefix, currentStr)=> {
+        let i = 0;
+        while (prefix[i] && prefix[i] === currentStr[i]){
+            i++
+        }
+        return prefix.slice(0,i)
+    });
 }
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
+console.log(longestCommonPrefix(["cargo","carnivore","car"]))
+
