8feff82e9bf63942ad45a9f17d739aa009d5f48b
ChelseyReann <chelseyreann@gmail.com>
2023-05-24 16:14:20 -0400

    adds a different solution

--- sortedSquareArray.js
+++ sortedSquareArray.js
@@ -2,12 +2,24 @@
 //sample Input: array = [1, 2, 3, 5, 6, 8, 9]
 //sample Output: [1, 4, 9, 25, 36, 64, 81]
 
+// function sortedSquareArray(array) {
+//     let square;
+//     let ans = [];
+
+//     for (let i = 0; i < array.length; i++){
+//         square = array[i] * array[i];
+//         ans.push(square)
+//     }
+//     return ans
+// }
+
 function sortedSquareArray(array) {
     let square;
     let ans = [];
 
     for (let i = 0; i < array.length; i++){
-        square = array[i] * array[i];
+        //proper syntax with squaring
+        square = array[i] ** 2;
         ans.push(square)
     }
     return ans
