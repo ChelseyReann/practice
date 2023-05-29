6882600e5d81b4eabf0a4c7a68f6ca591aefb2ef
ChelseyReann <chelseyreann@gmail.com>
2023-05-29 12:06:02 -0400

    adds another problem and solution

---
+++ addOne.js
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+// Your solution for 01-addOne here:
+
+const addOne = (num) => {
+    let plusOne = num += 1
+    return plusOne
+}
+
+console.log(addOne(1))
+console.log(addOne(-5))
