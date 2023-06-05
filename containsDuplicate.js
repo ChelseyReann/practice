d91e2f4d243e22982b3c264d23b40c7327ff59ea
ChelseyReann <chelseyreann@gmail.com>
2023-06-05 15:33:40 -0400

    adds new problem and solution

---
+++ containsDuplicate.js
@@ -0,0 +1,13 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+let containsDuplicate = function(nums) {
+    let len = nums.length
+    for (let i = 0; i < len; i++){
+        for (let j = i + 1; j < len; j++){
+            if (nums[i] === nums[j]) {
+                return true
+            }
+        }
+    }
+    return false
+}
