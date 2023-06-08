802d04f7f772f871e7cffe630e4c9a9b68527870
ChelseyReann <chelseyreann@gmail.com>
2023-06-08 15:38:41 -0400

    adds solution to problem

---
+++ removeElement.js
@@ -0,0 +1,4 @@
+//Given an integer array nums and an integer val, remove all occurences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+//Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+//Change the array nums such that the first k elements of nums contain the elemnts which are not equla to val. the remaining elements of nums are not important as well as the size of nums.
+//return k
