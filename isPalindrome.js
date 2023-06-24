055a061f9796eef4a0359cb81542815cf8dbc66e
ChelseyReann <chelseyreann@gmail.com>
2023-06-24 18:50:06 -0400

    adds time complexity

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,15 +1,15 @@
 //A phrase is a palindrome if, after converting all uppercase letter into lowercase letters & removing all non-alphanumeric characters, it reads the same fowards and backwards. Alpsnumeric characters include letters & numbers.
 //given a string(s) return true if palindrome & false otherwise
 
-function isPalindrome(s){
-    if(s === ""){
-        return true;
+function isPalindrome(s){ //O(1)
+    if(s === ""){         //O(1)
+        return true;      //O(1)
     }
 
-    let cleanedS = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    let reversedS = cleanedS.split("").reverse().join("");
+    let cleanedS = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(); //O(n)
+    let reversedS = cleanedS.split("").reverse().join(""); //O(n)
 
-    return cleanedS === reversedS;
+    return cleanedS === reversedS; //O(n)
 }
 
 let s = "A man, a plan, a canal: Panama"
