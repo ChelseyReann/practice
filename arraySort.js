60dd2218df7e5d73e6f38cc45f2ded8246f35828
ChelseyReann <chelseyreann@gmail.com>
2023-06-28 18:32:02 -0400

    adds new problem and solution and includes time and space complexity

--- arraySort.js
+++ arraySort.js
@@ -1,9 +1,33 @@
 //Implement a function that sorts an array of integers in ascending or descending order. You can use any sorting algorithm such as bubble sort, insertion sort, or quicksort.
 
-function arrSort(arr){
-    let newArr = arr.sort((a,b) => a-b)
-    return newArr
+// function arrSort(arr){
+//     let newArr = arr.sort((a,b) => a-b)
+//     return newArr
+// }
+
+// let ar = [9, 2, 13, 20]
+// console.log(arrSort(arr))
+
+function removeDups(arr){
+    let freqMap = {}
+    let results = []
+
+    for(let i = 0; i < arr.length; i++){
+        if(!freqMap[arr[i]]){
+            freqMap[arr[i]] = 1
+        } else {
+            freqMap[arr[i]]++
+        }
+    }
+
+    for(let num in freqMap){
+        if(freqMap[num] === 1){
+            results.push(num)
+        }
+    }
+
+    return results
 }
 
-let arr = [9, 2, 13, 20]
-console.log(arrSort(arr))
+let arr = [9, 2, 13, 9, 20]
+console.log(removeDups(arr))
--- maximumElement.js
+++ maximumElement.js
@@ -1,6 +1,11 @@
 //Write a function that takes an array of integers as input and returns the maximum element in the array
 //Example arr = [1, 3, 21, 5, 8, 90] -> 90
 
+//set a variable equal to a number in the array
+//iterate through the array.
+//if the element we are iterating through is greater than the number we are storing - store that new higher number in that variable
+//once the loop is done - return that variable
+
 function maxElement(arr) {
   let max = arr[0];
 
