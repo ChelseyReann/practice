be4edadaf3f95548d3348c19ab04e94e1fb9aaac
ChelseyReann <chelseyreann@gmail.com>
2023-07-06 16:52:17 -0400

    adds a binary search solution

--- nextGreatestLetter.js
+++ nextGreatestLetter.js
@@ -29,3 +29,30 @@ let target2 = 'z'
 console.log(nextGreatestLetter(letters, target))
 console.log(nextGreatestLetter(letters1, target1))
 console.log(nextGreatestLetter(letters2, target2))
+
+function nextLetter(letters, target){
+    let start = 0
+    let end = letters.length - 1
+
+    if(letters[end] <= target){
+        return letters[0]
+    }
+
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2)
+
+        if(letters[mid] <= target){
+            low = mid + 1
+        } else {
+            if(letters[mid - 1] <= target){
+                return letters[mid]
+            }
+            end = mid - 1
+        }
+    }
+    return letters[start]
+}
+
+console.log(nextLetter(letters, target))
+console.log(nextLetter(letters1, target1))
+console.log(nextLetter(letters2, target2))
