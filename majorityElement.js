ae6a3539163cb2b821b87fbfd53047e9ba4a22b9
ChelseyReann <chelseyreann@gmail.com>
2023-07-06 17:38:52 -0400

    adds time and space complexity

--- majorityElement.js
+++ majorityElement.js
@@ -2,6 +2,13 @@
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Approach:
+create a freqMap
+create test case if nums only contains 1 element return that element
+iterate through the array
+store the number we are iterating through in the freqMap if it doesn't already exist and increase it's count if it does already exist
+return the element that has the highest frequency
  */
 
 function majorityElement(nums){
@@ -27,3 +34,6 @@ let nums3 = [1]
 console.log(majorityElement(nums))
 console.log(majorityElement(nums2))
 console.log(majorityElement(nums3))
+
+//Time Complexity - O(N)
+//Space Complexity - O(N)
