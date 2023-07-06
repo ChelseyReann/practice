827ca9b51d1165aff6ced4383af8351bc7ab01e9
ChelseyReann <chelseyreann@gmail.com>
2023-07-06 16:25:58 -0400

    adds solution to the problem

--- nextGreatestLetter.js
+++ nextGreatestLetter.js
@@ -4,3 +4,15 @@ You are given an array of characters letters that is sorted in non-decreasing or
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters
 */
 
+function nextGreatestLetter(letters, target){
+    for(let i = 0; i < letters.length; i++){
+        if(letters[i] > target){
+            return letters[i]
+        }
+    }
+    return letters[0]
+}
+
+let letters = ['c', 'f', 'j']
+let target = 'a'
+console.log(nextGreatestLetter(letters, target))
