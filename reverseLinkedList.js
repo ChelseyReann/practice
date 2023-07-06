7d8eb38db125bc07fe8312b1fba44cbbf4a4383d
ChelseyReann <chelseyreann@gmail.com>
2023-07-06 18:38:46 -0400

    adds a solution

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,3 +1,18 @@
 /* 
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 */
+
+function reverseLink(head){
+    let prev = null
+    let current = head
+    let next = null
+
+    while (current){
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+
+    return prev
+}
