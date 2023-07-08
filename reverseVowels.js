292f33a038890bb59f8c3a638b8ce872dce1c473
ChelseyReann <chelseyreann@gmail.com>
2023-07-08 16:37:04 -0400

    adds solution to the problem

--- reverseVowels.js
+++ reverseVowels.js
@@ -11,3 +11,28 @@ Approach:
 - once they both contain vowels introduce a current variable that is equal to the split array at the start and manually swap the values
  - join s back into a string and return the value
 */
+
+function reverseVowels(s){
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let start = 0
+    let end = s.length - 1
+    let split = s.split("")
+
+    while (start <= end){
+        if(!vowels.includes(s[start]) && !vowels.includes[s[end]]){
+            start++
+            end--
+        } else if (!vowels.includes(s[start]) && vowels.includes(s[end])){
+            start++
+        } else if (vowels.includes(s[start]) && !vowels.includes(s[end])){
+            end--
+        } else if(vowels.includes(s[start]) && vowels.includes(s[end])){
+            let current = s[start]
+            s[start] = s[end]
+            s[end] = current
+            start++
+            end--
+        }
+    }
+    return split.join("")
+}
