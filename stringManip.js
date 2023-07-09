22dad0923d5c1088ddbc678f9e1feb5a52a7acea
ChelseyReann <chelseyreann@gmail.com>
2023-07-09 15:31:15 -0400

    adds solution for new problem

--- stringManip.js
+++ stringManip.js
@@ -23,16 +23,41 @@ function matches(str1, str2){
     return counter
 }
 
-let str1 = "hello"
-let str2 = "hills"
-console.log(matches(str1, str2))
+// let str1 = "hello"
+// let str2 = "hills"
+// console.log(matches(str1, str2))
 
 //time complexity - O(N)
 //Space complexity - O(1)
 
 /*
 Implement a function that accepts two strings and returns true if they have at least one character in common at the same index, and false otherwise.
-Example Input: "apple", "orange"
+Example Input: "apple", "aura"
 Example Output: true
+Approach:
+- loop through str1
+- if the indices of both strings match return true
+- otherwise return false
 */
 
+function booleanMatch(str1, str2){
+    for (let i = 0; i < str1.length; i++){
+        if(str1[i] === str2[i]){
+            return true
+        }
+    }
+    return false
+}
+let str1 = "apple"
+let str2 = 'aura'
+let str3 = "orange"
+let str4 = 'banana'
+console.log(booleanMatch(str1,str2))
+console.log(booleanMatch(str3,str4))
+
+
+/*
+Given two strings str1 and str2, write a function to return the count of distinct characters that match at the same positions in both strings.
+Example Input: "hello", "helps"
+Example Output: 3
+*/
