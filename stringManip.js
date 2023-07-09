8d00b267203f2a5bfc3d66aba58fc2ad7b1759f0
ChelseyReann <chelseyreann@gmail.com>
2023-07-09 14:46:56 -0400

    adds solution to problem with my approach

--- stringManip.js
+++ stringManip.js
@@ -12,3 +12,18 @@ return the counter
 because of the double 'l' the freqMap will not work - don't know hoe to differenciate letter that are duplicates yet so implemented a counter and a simple for loop instead
 */
 
+function matches(str1, str2){
+    let counter = 0
+
+    for (let i = 0; i < str1.length; i++){
+        if(str1[i] === str2[i]){
+            counter++
+        }
+    }
+    return counter
+}
+
+let str1 = "hello"
+let str2 = "hills"
+console.log(matches(str1, str2))
+
