f7a62b5c77b29d5e6914d9747642a0155e355bf5
ChelseyReann <chelseyreann@gmail.com>
2023-07-09 14:37:24 -0400

    adds new problem and solution

--- maximumElement.js
+++ maximumElement.js
@@ -18,5 +18,28 @@ function maxElement(arr) {
   return max;
 }
 
+
+//check if number is less than max but greater than all other elements arr[i] < max && arr[i] > second
+function secondHighestElement(arr) {
+    let max = arr[0];
+    let second = arr[1]
+    
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        second = max;
+        max = arr[i];
+        console.log("max: " + max)
+        console.log("second: " + second)
+      } else if(arr[i] < max && arr[i] > second){
+        second = arr[i]
+        console.log("else if second: " + second)
+      }
+    }
+  
+    return second;
+  }
+
 let arr = [1, 3, 21, 5, 8, 90];
 console.log(maxElement(arr));
+console.log(secondHighestElement(arr))
