f2363677df18498d0266059d8bdc02b8469041f1
ChelseyReann <chelseyreann@gmail.com>
2023-07-09 16:33:57 -0400

    adds my approach to solving the problem

--- arrayManip.js
+++ arrayManip.js
@@ -3,4 +3,12 @@ Create a function that takes a list of integers and returns the indices of all e
 
 Example Input: [1, 2, 3, 4, 5, 6]
 Example Output: [1, 3, 5]
+
+Approach:
+- create an empty results array
+- loop through the array
+- check if the number is divisible by 2, if yes push that index into the results array
+- return results array
 */
+
+
