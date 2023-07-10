4404e05c93d49b3c5b84e927056d51a552d332be
ChelseyReann <chelseyreann@gmail.com>
2023-07-10 12:02:29 -0400

    adds my approach

--- ransomNote.js
+++ ransomNote.js
@@ -2,4 +2,11 @@
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
 Each letter in magazine can only be used once in ransomNote.
+
+approach:
+- create a freqMap
+- loop through the ransomNote and add it to the freqMap
+- loop through the magazine and decrease it's chars from the freqMap
+- loop through the freqMap and if the count of each character is more than 0 then return false
+- other wise return true
 */
