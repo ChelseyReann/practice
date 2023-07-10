d079f884affc4b48814801b4b6b2c18d7a3e1625
ChelseyReann <chelseyreann@gmail.com>
2023-07-10 18:11:29 -0400

    adds new problem and solution

--- arrayManip.js
+++ arrayManip.js
@@ -27,6 +27,37 @@ console.log(findIndex(arr))
 //time complexity - O(N)
 //space complexity - O(N)
 
+/*
+Write a function that accepts two arrays and returns the number of common elements between them. The function should compare the elements at the same index in both arrays and count the number of matches.
+
+Example Input: 
+arr1 = [1,2,5,8,10]
+arr2 = [0,2,5,9,11]
+
+Example Output:
+2
+
+Approach:
+ - create a count variable
+ - create a for loop and check if each index is equal if yes count++
+ - return count
+*/
+
+function indexMatch(arr1, arr2){
+    let count = 0
+
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] === arr2[i]){
+            count++
+        }
+    }
+    return count
+}
+
+let arr1 = [1,2,5,8,10]
+let arr2 = [0,2,5,9,11]
+console.log(indexMatch(arr1, arr2))
+
 
 
 
