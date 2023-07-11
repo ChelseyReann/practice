c6e1eaad3184dcab745e9ad52b3a27af05cf96e6
ChelseyReann <chelseyreann@gmail.com>
2023-07-11 14:42:30 -0400

    adds new problem

--- karat7_11.js
+++ karat7_11.js
@@ -60,3 +60,45 @@ console.log(course_position(enrollments1));
 //Space complexity - O(N)
 
 
+/*
+An activity at our university requires students to work in pairs. Given a list of student ID number, course name pairs, write a function that returns a collection of all possible pairs of student ID numbers.
+
+Sample input and output:
+
+enrollments1 = [
+  ["58", "Linear Algebra"],
+  ["94", "Art History"],
+  ["94", "Operating Systems"],
+  ["17", "Software Design"],
+  ["58", "Mechanics"],
+  ["58", "Economics"],
+  ["17", "Linear Algebra"],
+  ["17", "Political Science"],
+  ["94", "Economics"],
+  ["25", "Economics"],
+  ["58", "Software Design"],
+]
+
+Expected output: [
+  "58,17", 
+  "58,94",
+  "58,25",
+  "94,25",
+  "17,94",
+  "17,25"
+]
+
+enrollments2 = [
+  ["0", "Advanced Mechanics"],
+  ["0", "Art History"],
+  ["1", "Course 1"],
+  ["1", "Course 2"],
+  ["0", "Computer Architecture"],
+]
+
+Expected output: [
+  "0,1"
+]
+*/
+
+
