fdada3833799cc92ea5f48992782119ed7b92154
ChelseyReann <chelseyreann@gmail.com>
2023-07-11 14:49:03 -0400

    creates function to solve problem and includes test cases

--- karat7_11.js
+++ karat7_11.js
@@ -19,11 +19,11 @@ const schedule1 = [
     }
   }
   
-  console.log(course_position(schedule1, "Linear Algebra"));
-  console.log(course_position(schedule1, "Art History"));
-  console.log(course_position(schedule1, "Political Science"));
-  console.log(course_position(schedule1, "Economics"));
-  console.log(course_position(schedule2, "Political Science"));
+//   console.log(course_position(schedule1, "Linear Algebra"));
+//   console.log(course_position(schedule1, "Art History"));
+//   console.log(course_position(schedule1, "Political Science"));
+//   console.log(course_position(schedule1, "Economics"));
+//   console.log(course_position(schedule2, "Political Science"));
 //Time complexity - O(N)
 //Space complexity - O(1)
 
@@ -44,18 +44,18 @@ const enrollments1 = [
     ["58", "Software Design"]
   ];
 
-function course_position(arr) {
-  let results = [];
+// function course_position(arr) {
+//   let results = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!results.includes(arr[i][0])) {
-      results.push(arr[i][0]);
-    }
-  }
-  return results;
-}
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!results.includes(arr[i][0])) {
+//       results.push(arr[i][0]);
+//     }
+//   }
+//   return results;
+// }
 
-console.log(course_position(enrollments1));
+// console.log(course_position(enrollments1));
 //Time complexity - O(N^2)
 //Space complexity - O(N)
 
@@ -100,5 +100,47 @@ Expected output: [
   "0,1"
 ]
 */
+
+const enrollments = [
+    ["58", "Linear Algebra"],
+    ["94", "Art History"],
+    ["94", "Operating Systems"],
+    ["17", "Software Design"],
+    ["58", "Mechanics"],
+    ["58", "Economics"],
+    ["17", "Linear Algebra"],
+    ["17", "Political Science"],
+    ["94", "Economics"],
+    ["25", "Economics"],
+    ["58", "Software Design"],
+  ];
+  
+  const enrollments2 = [
+    ["0", "Advanced Mechanics"],
+    ["0", "Art History"],
+    ["1", "Course 1"],
+    ["1", "Course 2"],
+    ["0", "Computer Architecture"],
+  ];
+  
+  function course_position(arr){
+    let results = []
+    let freqMap = {}
+
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            let pair = [arr[i][0], arr[j][0]].sort()
+            let joinedPair = pair.join(",")
+            if(!freqMap[joinedPair] && arr[i][0] !== arr[j][0]){
+                freqMap[joinedPair] = true
+                results.push(pair)
+            }
+        }
+    }
+    return results
+  }
+  
+  console.log(course_position(enrollments))
+  console.log(course_position(enrollments2));
 
 
