5fd30af9ba3ae75a458ad63b62520a87a92c1802
ChelseyReann <chelseyreann@gmail.com>
2023-07-12 15:38:49 -0400

    adds new problem

---
+++ buddyStrings.js
@@ -0,0 +1,16 @@
+/*
+Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+Example Input:
+let s = "ab"
+let goal = "ba"
+
+Example Output:
+true
+
+Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+*/
