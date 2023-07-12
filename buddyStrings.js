e6c093ccaa90f942262b535ef1c0b0467317520e
ChelseyReann <chelseyreann@gmail.com>
2023-07-12 15:44:21 -0400

    adds my approach to solving the problem

--- buddyStrings.js
+++ buddyStrings.js
@@ -13,4 +13,16 @@ Example Output:
 true
 
 Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+
+Approach:
+- check if the strings are the same length, return false if not
+- check if s === goal
+ - adding chars in s to a freqMap
+ - if there are dupes in s that means a swap can happen and we can return true
+ - otherwise return false
+-loop through s and check for when s !== goal
+- store these chars in a array
+- check if this array is !== 2 in length - because if it's not return false because a proper swap is not happening
+- destructure the mismatched array and set it equal to mismatchedIndices
+- return s[idx1] === goal[idx2] && s[idx2] === goal[idx1]
 */
