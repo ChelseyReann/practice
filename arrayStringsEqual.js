263db5150e37f9b07f01ab59a24bb04bd7438f3b
ChelseyReann <chelseyreann@gmail.com>
2023-07-13 14:21:05 -0400

    adds approach and creates a function to solve the problem

--- arrayStringsEqual.js
+++ arrayStringsEqual.js
@@ -2,4 +2,19 @@
 Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
 
 A string is represented by an array if the array elements concatenated in order forms the string.
+
+Approach:
+- join the strings within each array
+- return if the joined strs are === to each other
 */
+
+function arrayStringsEqual(words1, words2){
+   const str1 = words1.join("")
+   const str2 = words2.join("")
+   
+   return str1 === str2
+}
+
+let words1 = ['ab', 'c']
+let words2 = ['a', 'bc']
+console.log(arrayStringsEqual(words1, words2))
