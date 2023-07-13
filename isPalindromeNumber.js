56ce3c3a666c202b7a9265aca3d10e029769d64f
ChelseyReann <chelseyreann@gmail.com>
2023-07-13 12:06:28 -0400

    adds my approach to solving the problem

--- isPalindromeNumber.js
+++ isPalindromeNumber.js
@@ -1,5 +1,9 @@
 /*
-Given an integer x, return true if x is a 
-palindrome
-, and false otherwise.
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Approach: 
+- create a variable and change x into a string so I can split, reverse and join it back together
+- return if modX === x
 */
+
+
