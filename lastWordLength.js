50465d2399b42931fa7b6b2e6168603acae0e1c1
ChelseyReann <chelseyreann@gmail.com>
2023-07-13 13:35:43 -0400

    creates function to solve problem and test cases

--- lastWordLength.js
+++ lastWordLength.js
@@ -4,3 +4,12 @@ Given a string s consisting of words and spaces, return the length of the last w
 A word is a maximal 
 substring consisting of non-space characters only.
 */
+
+function lastWordLength(s){
+   let trimmedString = s.trim()
+
+   return trimmedString.length - trimmedString.lastIndexOf(" ") - 1
+}
+
+let s = "Hello World"
+console.log(lastWordLength(s))
