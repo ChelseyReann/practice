232186c965202c8edb9eb94bc9506be56db49024
ChelseyReann <chelseyreann@gmail.com>
2023-07-14 15:30:58 -0400

    adds my approach to solving the problem

--- moveZeros.js
+++ moveZeros.js
@@ -3,4 +3,12 @@ Write a method that moves all zeros in an array to its end. You should maintain 
 
 zerosToEnd([1, 0, 2, 0, 4, 0])
 // [1, 2, 4, 0, 0, 0]
+
+Approach:
+- two pointers method
+- while start < end
+- if nums[start] === 0
+- make nums[start] = nums[end]
+- increment start and decrement end
+- return array
 */
