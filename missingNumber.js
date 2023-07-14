2986d0cb78903c1db9a2ca14925bdad9297dbc63
ChelseyReann <chelseyreann@gmail.com>
2023-07-14 14:22:07 -0400

    adds my approach to solving the problem

--- missingNumber.js
+++ missingNumber.js
@@ -6,4 +6,11 @@ However, we notice that there are some missing numbers in the sequence.
 [1, 2, 4, 5, 7]
 
 Can you write a method missingNumbers that takes an array of continuous numbers and returns the missing integers?
+
+Approach:
+- create an empty results array
+- loop through the array
+- check if the next number is nums[i] + 1
+- if not push nums[i] + 1 into the results array
+- return the results array
 */
