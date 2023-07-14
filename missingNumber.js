21885a76901beef436860cb64dd2ba9f776a7342
ChelseyReann <chelseyreann@gmail.com>
2023-07-14 14:59:37 -0400

    creates function to solve problem and test cases

--- missingNumber.js
+++ missingNumber.js
@@ -10,7 +10,29 @@ Can you write a method missingNumbers that takes an array of continuous numbers 
 Approach:
 - create an empty results array
 - loop through the array
+- need a nested for loop?
 - check if the next number is nums[i] + 1
 - if not push nums[i] + 1 into the results array
 - return the results array
 */
+
+function missingNums(nums){
+    let results = []
+
+    for(let i = 0; i < nums.length; i++){
+            if(nums[i + 1] !== nums[i] + 1){
+                let current = nums[i] + 1
+                results.push(current)
+            } 
+            if(nums[i + 1] === nums[nums.length - 1]){
+                break
+            }
+        
+    }
+    return results
+}
+
+let nums = [1,2,4,5,7]
+let nums2 = [3,5,7,9]//[4,6,8]
+console.log(missingNums(nums))
+console.log(missingNums(nums2))
