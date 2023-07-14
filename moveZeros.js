d88974dbdebd5924dcf7a8c53ee655fe504d5c56
ChelseyReann <chelseyreann@gmail.com>
2023-07-14 15:29:30 -0400

    adds new problem

---
+++ moveZeros.js
@@ -0,0 +1,6 @@
+/* 
+Write a method that moves all zeros in an array to its end. You should maintain the order of all other elements. Here's an example:
+
+zerosToEnd([1, 0, 2, 0, 4, 0])
+// [1, 2, 4, 0, 0, 0]
+*/
