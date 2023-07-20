ed2908bd1be9755c47b3ed6cfd71b6a1201deed5
ChelseyReann <chelseyreann@gmail.com>
2023-07-20 11:00:46 -0400

    creates function to solve problem and test cases

--- mathSq.js
+++ mathSq.js
@@ -1,3 +1,12 @@
 /*
 Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 */
+
+function mathSq(x){
+    return Math.floor(Math.sqrt(x))
+}
+
+let x = 4
+let x2 = 8
+console.log(mathSq(x))
+console.log(mathSq(x2))
