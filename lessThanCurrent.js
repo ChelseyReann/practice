409f3fd3aacf093710688e726019d5f2e263e1ef
ChelseyReann <chelseyreann@gmail.com>
2023-07-24 15:40:42 -0400

    fixies the logic in my function so it outputs correctly

--- lessThanCurrent.js
+++ lessThanCurrent.js
@@ -20,7 +20,7 @@ function lessThanCurrent(nums){
     for(let i =0; i < nums.length; i++){
         let counter = 0
         for(let j = 0; j < nums.length; j++){
-            if(nums[i] < nums[j]){
+            if(nums[i] > nums[j]){
                 counter++
             }
         }
@@ -32,3 +32,6 @@ function lessThanCurrent(nums){
 let nums = [8,1,2,2,3] //[4,0,1,1,3]
 let nums2 = [6,5,4,8] //[2,1,0,3]
 let nums3 = [7,7,7,7] //[0,0,0,0]
+console.log(lessThanCurrent(nums))
+console.log(lessThanCurrent(nums2))
+console.log(lessThanCurrent(nums3))
