f7b99d47e4c964728b65cf704863f62b05a9a525
ChelseyReann <chelseyreann@gmail.com>
2023-07-26 18:26:35 -0400

    adds my approach

--- uniqueSum.js
+++ uniqueSum.js
@@ -2,4 +2,14 @@
 You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
 
 Return the sum of all the unique elements of nums
+
+Approach:
+- create an empty freqMap
+- create a sum variable set it equal to 0
+- loop through nums storing nums in the freqMap
+- loop through freqMap seeing where the nums === 1
+- add parseInt(num) to sum
+- return sum
 */
+
+
