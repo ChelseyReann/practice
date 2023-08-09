aec66852546537c123f59e3cf805d0a037f9c9bb
ChelseyReann <chelseyreann@gmail.com>
2023-08-09 19:32:46 -0400

    adds example input and output

--- repeatedNTimes.js
+++ repeatedNTimes.js
@@ -5,4 +5,9 @@ nums.length == 2 * n.
 nums contains n + 1 unique elements.
 Exactly one element of nums is repeated n times.
 Return the element that is repeated n times.
+
+Example 1:
+
+Input: nums = [1,2,3,3]
+Output: 3
 */
